cb9ded39666def35253fec74714ff6294064f983
Rebecca Mutek <mutek.b@gmail.com>
2018-02-02 14:50:12 -0500

    set up the scoreboard with players and beginning score

--- app.jsx
+++ app.jsx
@@ -1,6 +1,64 @@
 function Application () {
   return (
-    <div>
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+
+          <div className="player-name">
+            Rebecca 
+          </div>
+
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 24 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="players">
+        <div className="player">
+
+          <div className="player-name">
+            Tanya
+          </div>
+
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 26 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="players">
+        <div className="player">
+
+          <div className="player-name">
+            Tara
+          </div>
+
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 25 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
     </div>
     );
 }
