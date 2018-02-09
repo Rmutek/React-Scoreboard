3557a5d380ae67813ce171290f4a1fe1a968bfa1
Rebecca Mutek <mutek.b@gmail.com>
2018-02-09 09:23:49 -0600

    added a players component

--- app.jsx
+++ app.jsx
@@ -11,8 +11,32 @@ Header.propTypes = {
 };
 
 function Player(props) {
-  
+  return (
+      <div className="players">
+        <div className="player">
+
+          <div className="player-name">
+            {props.name} 
+          </div>
+
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> {props.score} </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    );
 }
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  socre: React.PropTypes.number.isRequired,
+};
 
 function Application(props) {
   return (
@@ -20,21 +44,9 @@ function Application(props) {
       <Header title={props.title} />
 
       <div className="players">
-        <div className="player">
-
-          <div className="player-name">
-            Rebecca 
-          </div>
-
-          <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 24 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-
-        </div>
+        <Players name="Rebecca" score={24} />
+        <Players name="Tanya" score={27} />
+        <Players name="Tara" score={28} />
       </div>
 
       <div className="players">
