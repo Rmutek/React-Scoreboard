7a6e9f9a5ac68b5ca08b107c7cb267c2979cd608
Rebecca Mutek <mutek.b@gmail.com>
2018-02-09 12:32:12 -0600

    added the decrement method to the application and added a way to set the inital score for each player

--- app.jsx
+++ app.jsx
@@ -30,12 +30,12 @@ Header.propTypes = {
 
 var Counter = React.createClass({
   propTypes: {
-    socre: React.PropTypes.number.isRequired,
+    initialScore: React.PropTypes.number.isRequired,
   },
 
   getInitialState: function() {
     return {
-      score: 0,
+      score: this.props.initialScore,
     }
   },
 
@@ -45,10 +45,16 @@ var Counter = React.createClass({
     })
   },
 
+  decrementScore: function(e) {
+    this.setState({
+      score: (this.state.score - 1),
+    })
+  },
+
   render: function(){
     return (
     <div className="counter">
-      <button className="counter-action decrement"> - </button>
+      <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
         <div className="counter-score"> {this.props.score} </div>
       <button className="counter-action increment" onClick={this.incrementScore}> + </button>
     </div>
@@ -66,7 +72,7 @@ function Player(props) {
           </div>
 
           <div className="player-score">
-            <Counter score= {props.score} />
+            <Counter initialScore={props.score} />
           </div>
 
         </div>
