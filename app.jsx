1e62cc97d38b35fb27f4101c8612941b34d57c07
Rebecca Mutek <mutek.b@gmail.com>
2018-02-09 10:35:48 -0600

    added counter component

--- app.jsx
+++ app.jsx
@@ -10,6 +10,21 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+        <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+
+    );
+}
+
+Counter.propTypes = {
+  socre: React.PropTypes.number.isRequired,
+};
+
 function Player(props) {
   return (
       <div className="players">
@@ -20,11 +35,7 @@ function Player(props) {
           </div>
 
           <div className="player-score">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> {props.score} </div>
-              <button className="counter-action increment"> + </button>
-            </div>
+            <Counter score= {props.score} />
           </div>
 
         </div>
@@ -63,7 +74,7 @@ Application.defaultProps = {
 };
 
 
-ReactDOM.render(<Application/>, document.getElementById('container'));
+ReactDOM.render(<Application />, document.getElementById('container'));
 
 
 
